e6d7f9d8c66d997af251864aa14424aa41317fef
Victor Nyingi Muchiri <nyingimuchiriv@gmail.com>
2024-07-31 22:04:00 +0300

    edited the forms

--- src/components/user/RegistrationForm.jsx
+++ src/components/user/RegistrationForm.jsx
@@ -99,7 +99,7 @@ function RegistrationForm({
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+                className="group relative w-full max-w-xs mx-auto flex justify-center py-2 px-6 border border-transparent text-sm font-medium rounded-full text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
               >
                 REGISTER
               </button>
