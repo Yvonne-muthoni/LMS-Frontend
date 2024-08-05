25043fa826ef39a82822e97915b5d3f466f0fb59
najibdevs <najibsoftwareeng@gmail.com>
2024-08-05 22:17:44 +0100

    fix labs (student home page)

--- src/components/home/RecommendedSection.jsx
+++ src/components/home/RecommendedSection.jsx
@@ -24,7 +24,7 @@ function RecommendedSection() {
 
   return (
     <section className="my-16">
-      <h3 className="text-3xl font-semibold mb-8 text-center">Recommended Courses</h3>
+      <h3 className="text-3xl font-semibold mb-10 text-center">Recommended Courses</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {courses.map((course) => {
           return (
