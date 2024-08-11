5126beac649327fb52dcc6aaa502305064d165d6
Victor Nyingi Muchiri <nyingimuchiriv@gmail.com>
2024-08-11 17:23:16 +0300

    done some changes on the pages

--- src/components/dashboards/admin/NavigationSidebar.jsx
+++ src/components/dashboards/admin/NavigationSidebar.jsx
@@ -8,17 +8,8 @@ const NavigationSidebar = () => (
       <Link to="/admin-dashboard" className="flex items-center py-2.5 px-4 rounded transition duration-200 hover:bg-[#FF3E3E]">
         <FaTachometerAlt className="mr-3" /> Dashboard
       </Link>
-      <Link to="/user-management" className="flex items-center py-2.5 px-4 rounded transition duration-200 hover:bg-[#FF3E3E]">
-        <FaUsers className="mr-3" /> User Management
-      </Link>
       <Link to="/course-management" className="flex items-center py-2.5 px-4 rounded transition duration-200 hover:bg-[#FF3E3E]">
         <FaBook className="mr-3" /> Course Management
-      </Link>
-      <Link to="/notifications" className="flex items-center py-2.5 px-4 rounded transition duration-200 hover:bg-[#FF3E3E]">
-        <FaBell className="mr-3" /> Notifications
-      </Link>
-      <Link to="/settings" className="flex items-center py-2.5 px-4 rounded transition duration-200 hover:bg-[#FF3E3E]">
-        <FaCog className="mr-3" /> System Settings
       </Link>
     </nav>
   </aside>
