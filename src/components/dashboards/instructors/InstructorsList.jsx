d2e163b6d334c13001f20e208b49133a2a696abb
Victor Nyingi Muchiri <nyingimuchiriv@gmail.com>
2024-08-15 16:26:27 +0300

    fixed error in the instructors list

--- src/components/dashboards/instructors/InstructorsList.jsx
+++ src/components/dashboards/instructors/InstructorsList.jsx
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { UserIcon } from '@heroicons/react/24/solid'; 
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { UserIcon } from "@heroicons/react/24/solid";
 
 const InstructorsList = () => {
   const [adminUsers, setAdminUsers] = useState([]);
